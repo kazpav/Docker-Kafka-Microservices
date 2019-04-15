fe81bdcaa452084dd197a051b60adf6c15ac4110
Pavel-Valdemar Kazarin <x.pasha6@gmail.com>
2019-04-15 13:29:09 +0300

    Dao layer updated

--- dao_service/src/main/java/com/practice/dakm/daoservice/model/User.java
+++ dao_service/src/main/java/com/practice/dakm/daoservice/model/User.java
@@ -1,7 +1,11 @@
 package com.practice.dakm.daoservice.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection = "user")
 public class User {
+    @Id
     private String id;
     private String name;
     private String email;
--- dao_service/src/main/java/com/practice/dakm/daoservice/service/KafkaConsumerServiceImpl.java
+++ dao_service/src/main/java/com/practice/dakm/daoservice/service/KafkaConsumerServiceImpl.java
@@ -1,11 +1,19 @@
 package com.practice.dakm.daoservice.service;
 
+import com.practice.dakm.daoservice.dao.UserRepository;
 import com.practice.dakm.daoservice.model.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaConsumerServiceImpl {
+    private static final Logger logger = LogManager.getLogger(KafkaConsumerServiceImpl.class);
+
+    @Autowired
+    private UserRepository userRepository;
 
     @KafkaListener(topics = "Kafka_Example", id = "group_id")
     public void consume(String message) {
@@ -14,6 +22,7 @@ public class KafkaConsumerServiceImpl {
 
     @KafkaListener(topics = "Kafka_Example_json", id = "group_json", containerFactory = "userKafkaListenerFactory")
     public void consumeJson(User user) {
-        System.out.println(user);
+        logger.info("Recieved user: {}", user);
+        userRepository.save(user);
     }
 }
